98b4acbed7c8fcaa1fa42847b1429094e66d4ec5
xanf-code <darshanaswathh@gmail.com>
2022-11-13 22:43:47 +0530

    🚀

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 - Downloads images from a given URL (Cloudinary)
 - Saves images to a given directory
 - Picked up by another script for posting on instagram
+
+- Interactions
+- [x] Like recent posts hashtags
+- [x] Follow users of a particular user
+- [x] Comment on posts under hashtags
+- [ ] Unfollow users who don't follow back
--- index.py
+++ index.py
@@ -3,20 +3,77 @@ import cloudinary
 import cloudinary.api
 import urllib.request
 import requests
+from pathlib import Path
+import shutil
+import time
+from instagrapi import Client
+import random
+
+# Hashtag List
+hashtags = [
+    "writingcommunity",
+    "moonquotes",
+    "lyricedits",
+    "poetsdaily",
+    "vibez",
+    "relatabletweets",
+    "relatablequote",
+    "sadtweets",
+    "aesthetic",
+    "tumblrquotes",
+    "instaquote",
+    "bookquote",
+    "aestheticquotes",
+    "relatableteen",
+    "relatabletextposts",
+    "sadquotespage",
+    "lifequotestagram",
+    "moodquotes",
+    "mood",
+    "icanrelate",
+    "selfcarelove",
+    "loveyouself",
+    "sassyquotes",
+    "relatable",
+    "loverelationship",
+    "tumblr",
+    "healing",
+]
+
+# Followers
+followers = [
+    "22034694471",
+    "9294170459",
+    "39801559480",
+    "25355898574",
+    "45370826080",
+    "24850497158"
+]
+
+comments = ["Really nice.", "I like this.", "Nice.", "OMG.", "Great feed!", "Reminds me of something ...", "Exellent.", "Love it.", "😀😀😀",
+            "😃😃😃", "😄😃😀", "😁😁😁", "😆😆😆", "😅😅😅", "😂😂😂", "😊😊😊", "😇😇😇", "🙂🙂🙂🙂🙂", "😎 😎 😎", "😍😍😍", "😳😳😳", "😱😱😱", "👀", "💪💪💪", "🤘 🤘 🤘", "👍👍👍"
+            ]
 
 # Setup cloudinary configuration
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# Instagrapi config
+cl = Client()
+cl.login(config['instagram']['insta_username'],
+         config['instagram']['insta_password'])
+
 cloudinary.config(
-    cloud_name = config['cloudinary']['cloud_name'],
-    api_key = config['cloudinary']['api_key'],
-    api_secret = config['cloudinary']['api_secret']
+    cloud_name=config['cloudinary']['cloud_name'],
+    api_key=config['cloudinary']['api_key'],
+    api_secret=config['cloudinary']['api_secret']
 )
+
 
 def getUrl():
     image_info = cloudinary.api.resource('next_post')
     return image_info['secure_url']
+
 
 def downloadImage():
     pic_url = getUrl()
@@ -32,8 +89,70 @@ def downloadImage():
 
             handle.write(block)
 
+
+def uploadImagetoInstagam():
+    cl.photo_upload(path="next_post.jpg",
+                    caption="""
+                 Follow @3.am.__.talks for more relatable quotes ✨🥀
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+#writingcommunity #moonquotes #lyricedits #poetsdaily #vibez #relatabletweets #relatablequote #sadtweets #aesthetic #tumblrquotes #instaquote #bookquote #aestheticquotes #relatableteen #relatabletextposts #sadquotespage #lifequotestagram #moodquotes #mood #icanrelate #selfcarelove #loveyouself #sassyquotes #relatable #loverelationship #tumblr #healing
+                 """)
+
+
+def likehashtags():
+    for hashtag in hashtags:
+        medias = cl.hashtag_medias_top(hashtag, amount=1)
+        for media in medias:
+            cl.media_like(media.id)
+            time.sleep(3)
+
+def userFollowers():
+    for follower in followers:
+        users = cl.user_followers(follower, amount=3)
+        for user in users:
+            cl.user_follow(user)
+            time.sleep(3)
+
+def commentOnMedia():
+    comment = random.choice(comments)
+    for hashtag in hashtags:
+        medias = cl.hashtag_medias_top(hashtag, amount=1)
+        for media in medias:
+            cl.media_comment(media.id, comment)
+            time.sleep(3)
+            
+def instagramEngagement():
+    # Get Most recent posts by Hashtag
+    likehashtags()
+    print("Liking hashtags Successful")
+    time.sleep(5)
+    # Follow users of a particular user
+    userFollowers()
+    print("Following users Successful")
+    time.sleep(5)
+    # Comment on posts
+    commentOnMedia()
+    print("Commenting on posts Successful")
+    time.sleep(5)
+
 def main():
     downloadImage()
-    print("Image Downloaded Successfully")
-    
+    print("--------Image Downloaded Successfully--------")
+    uploadImagetoInstagam()
+    print("--------Image Uploaded Successfully--------")
+    instagramEngagement()
+    print("--------Instagram Engagement Successful--------")
+
+
 main()
